e8e703fec1815d56fd805f22aa4822ed368943e0
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-05-06 12:31:39 +0000

    make_edit.view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,16 +3,16 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
      redirect_to book_path(@book.id)
     else
      render :new
-    end 
-  end 
-  
+    end
+  end
+
   def index
   end
 
@@ -21,8 +21,9 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
 private
   def book_params
     params.require(:book).permit(:title, :body)
