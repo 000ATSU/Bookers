95a4c4bfd2ff161deda307dd2ce0fdc0a9a8f23c
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-05-06 13:37:59 +0000

    make_destroy.action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-    @books = Book.all
-  end
-
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -11,6 +6,11 @@ class BooksController < ApplicationController
     else
      render :new
     end
+  end
+  
+  def new
+    @book = Book.new
+    @books = Book.all
   end
 
   def index
@@ -23,7 +23,19 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id]) 
+    book.destroy
+    redirect_to new_book_path
+  end
+  
 private
   def book_params
     params.require(:book).permit(:title, :body)
