dced2beaf7a64d204fdd33a4f6a23d23605316b3
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-05-07 08:21:53 +0000

    Finish_Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,13 +5,8 @@ class BooksController < ApplicationController
     if @book.save
      redirect_to book_path(@book.id), notice: 'Book was successfully created'
     else
-     render :new
+     render :index
     end
-  end
-  
-  def new
-    # @book = Book.new
-    # @books = Book.all
   end
 
   def index
@@ -26,22 +21,24 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-   if book.update(book_params)
-    redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
    else
-    render :edit 
-   end  
+    flash[:notice] = 'update error'
+    render :edit
+
+   end
   end
-  
+
   def destroy
-    book = Book.find(params[:id]) 
+    book = Book.find(params[:id])
     book.destroy
     redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
-  
+
 private
   def book_params
     params.require(:book).permit(:title, :body)
